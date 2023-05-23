4bfccb7582b8dd2f8b808ae3e8175fb34700a0aa
SimonDesc <pro.descombes@gmail.com>
2023-05-23 18:05:21 +0200

    feat: add new selection sort function

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+/**
+ * selection_sort - implements the selection sort algorithm
+ * to sort an array of integers in ascending order.
+ *
+ * @array: a pointer to the first element of the array to be sorted
+ * @size: The size parameter is the number of elements in the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned long int i = 0, j = 0, jMin = 0, tmp_i = 0;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		jMin = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[jMin])
+			{
+				jMin = j;
+			}
+		}
+
+		if (jMin != i)
+		{
+			tmp_i = array[i];
+			array[i] = array[jMin];
+			array[jMin] = tmp_i;
+		}
+	}
+}
