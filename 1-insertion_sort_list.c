4c5765629857e1dfe2cd10edf307b8af14ab8980
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-24 09:31:18 +0200

    fix the function to print the right output

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,36 +11,33 @@ void insertion_sort_list(listint_t **list)
 {
     listint_t *i = NULL;
     listint_t *j = NULL;
-    listint_t *next = NULL;  // new variable to track the next node
 
     if (list == NULL || *list == NULL)
         return;
 
-    for (i = (*list)->next; i != NULL; i = next)
+    for (i = (*list)->next; i != NULL; i = i->next)
     {
-        next = i->next;  // update next before doing the swap
-
-        for (j = i; j != NULL && j->prev != NULL; j = j->prev)
+        for (j = i->prev; j != NULL; j = j->prev)
         {
             if ((i->n) < (j->n))
             {
-                // i is not the last element in the list
+                /* i is not the last element in the list */
                 if (i->next)
                     i->next->prev = j;
 
-                // j is not the first element in the list
+                /* j is not the first element in the list */
                 if (j->prev)
                     j->prev->next = i;
-                else // j is the first element, update the head of the list
+                else /* j is the first element, update the head of the list */
                     *list = i;
 
-                // Swap i and j
+                /* Swap i and j */
                 i->prev = j->prev;
                 j->next = i->next;
                 i->next = j;
                 j->prev = i;
 
-                // Print the list
+                /* Print the list */
                 print_list(*list);
             }
         }
