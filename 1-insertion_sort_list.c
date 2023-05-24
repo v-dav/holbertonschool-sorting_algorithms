25e548f0647c19c5370f498593166fdf3ad22e34
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-24 09:32:50 +0200

    fix betty style

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,38 +9,37 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *i = NULL;
-    listint_t *j = NULL;
+	listint_t *i = NULL;
+	listint_t *j = NULL;
 
-    if (list == NULL || *list == NULL)
-        return;
+	if (list == NULL || *list == NULL)
+		return;
 
-    for (i = (*list)->next; i != NULL; i = i->next)
-    {
-        for (j = i->prev; j != NULL; j = j->prev)
-        {
-            if ((i->n) < (j->n))
-            {
-                /* i is not the last element in the list */
-                if (i->next)
-                    i->next->prev = j;
+	for (i = (*list)->next; i != NULL; i = i->next)
+	{
+		for (j = i->prev; j != NULL; j = j->prev)
+		{
+			if ((i->n) < (j->n))
+			{
+				/* i is not the last element in the list */
+				if (i->next)
+					i->next->prev = j;
 
-                /* j is not the first element in the list */
-                if (j->prev)
-                    j->prev->next = i;
-                else /* j is the first element, update the head of the list */
-                    *list = i;
+				/* j is not the first element in the list */
+				if (j->prev)
+					j->prev->next = i;
+				else /* j is the first element, update the head of the list */
+					*list = i;
 
-                /* Swap i and j */
-                i->prev = j->prev;
-                j->next = i->next;
-                i->next = j;
-                j->prev = i;
+				/* Swap i and j */
+				i->prev = j->prev;
+				j->next = i->next;
+				i->next = j;
+				j->prev = i;
 
-                /* Print the list */
-                print_list(*list);
-            }
-        }
-    }
+				/* Print the list */
+				print_list(*list);
+			}
+		}
+	}
 }
-
