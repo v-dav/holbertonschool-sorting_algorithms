5cbb123ce0657f59fa21609eaaec62c06db7e884
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-05-24 10:49:43 +0200

    write the first draft of the quick_sort function

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+ * quick_sort - function that implements the quick sort algorithm
+ * to sort an array of integers in ascending order.
+ *
+ * @array: a pointer to the first element of the array to be sorted
+ * @size: The size parameter is the number of elements in the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (!array)
+		return;
+}
